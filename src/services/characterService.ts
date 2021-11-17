a168f05fa70ce871408e3967b1566e1431cba810
Vishnu Paturu <vishnu.paturu@happiestminds.com>
2021-11-17 16:43:02 +0530

    added get api

--- src/services/characterService.ts
+++ src/services/characterService.ts
@@ -1,11 +1,19 @@
+import { inject } from 'aurelia-dependency-injection';
 import {HttpClient} from 'aurelia-fetch-client';
-
+import md5 from 'crypto-js/md5';
+@inject(HttpClient)
 export class CharacterService {
-    constructor(private http: HttpClient) {
-
+    private http: HttpClient;
+    constructor(http) {
+        this.http = http;
+        const baseUrl = 'https://gateway.marvel.com/v1/public/';
+        this.http.configure(c => {
+            c.withBaseUrl(baseUrl)
+        })
     }
 
     public getCharacters() {
-        console.log('api will be called here')
+        const pub = '58adebe21ff3a1cd284ce2ee8a1d13a6';
+        return this.http.fetch(`characters?apikey=${pub}`);
     }
 }
--- src/views/characters/characters.ts
+++ src/views/characters/characters.ts
@@ -3,10 +3,13 @@ import { CharacterService } from './../../services/characterService';
 
 @inject(CharacterService)
 export class Characters {
+    characters: any;
     constructor(private cs: CharacterService) {
     }
     
     attached() {
-        this.cs.getCharacters()
+        this.cs.getCharacters().then(d => {
+            d.json().then(r => this.characters = r.data.results)
+        }).catch(e => console.log(e))
     }
 }
