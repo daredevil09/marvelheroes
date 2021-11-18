d9625957c14aa1e8260d2b61541f6e4d8dda5d1a
Vishnu Paturu <vishnu.paturu@happiestminds.com>
2021-11-18 17:12:04 +0530

    infinite scrolling

--- src/components/cards-list/cards-list.ts
+++ src/components/cards-list/cards-list.ts
@@ -1,27 +1,47 @@
-import { inject } from 'aurelia-dependency-injection';
-import { bindable } from 'aurelia-framework';
-import { HttpService } from './../../services/http-service';
+import { inject } from "aurelia-dependency-injection";
+import { bindable } from "aurelia-framework";
+import { HttpService } from "./../../services/http-service";
 
 @inject(HttpService)
 export class CardsList {
-    @bindable category;
-    public data;
-    public searchQuery;
-    public loading = true;
-    constructor(private http: HttpService) {
-    }
-    
-    attached() {
-        this.getData()
-    }
+  @bindable category;
+  public data = [];
+  public searchQuery;
+  public loading = true;
+  constructor(private http: HttpService) {}
 
-    public getData() {
-        this.loading = true;
-        this.http.getData(this.category.url, this.searchQuery).then(d => {
-            d.json().then(r => {
-                this.data = r.data.results;
-                this.loading = false;
-            })
-        }).catch(e => console.log(e))
+  attached() {
+    this.getData();
+    window.addEventListener("scroll", this.scrollEvent.bind(this));
+  }
+
+  public getData(offSet?) {
+    if(!offSet) {
+      this.data = [];
     }
+    this.loading = true;
+    this.http
+      .getData(this.category.url, this.searchQuery, offSet)
+      .then((d) => {
+        d.json().then((r) => {
+          this.data = [...this.data, ...r.data.results];
+          this.loading = false;
+        });
+      })
+      .catch((e) => console.log(e));
+  }
+
+  private scrollEvent() {
+    const winScroll =
+      document.body.scrollTop || document.documentElement.scrollTop;
+
+    const height =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+
+    const scrolled = winScroll / height;
+    if(scrolled === 1) {
+        this.getData(this.data.length)
+    }
+  }
 }
--- src/services/http-service.ts
+++ src/services/http-service.ts
@@ -11,9 +11,9 @@ export class HttpService {
         })
     }
 
-    public getData(endpoint, searchQuery?) {
+    public getData(endpoint, searchQuery?, offSet = 0) {
         const pub = '58adebe21ff3a1cd284ce2ee8a1d13a6';
-        let api = `${endpoint}?apikey=${pub}&limit=50`;
+        let api = `${endpoint}?apikey=${pub}&limit=100&offset=${offSet}`;
         if(searchQuery) {
             api += endpoint === 'characters' ? '&nameStartsWith=' + searchQuery: '&titleStartsWith=' + searchQuery;
         }
